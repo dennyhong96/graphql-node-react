0b683e8b03d5552d65c0d55a3f8c05cba6ce2f04
Haiyang Hong <hong961127@gmail.com>
2020-08-02 14:18:09 -0700

    Define book schema

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
 
+const { graphqlHTTP } = require("express-graphql");
+
+const connectDB = require("./config/db");
+
 const app = express();
+connectDB();
+
+const schema = app.use("/graphql", graphqlHTTP({}));
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server up on port ${port}...`));
