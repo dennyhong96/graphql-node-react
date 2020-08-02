bb224e14cc1327b6359dd441c8ae6fea397f02af
Haiyang Hong <hong961127@gmail.com>
2020-08-02 16:19:48 -0700

    Setup BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import BookList from "./components/BookList";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div id="main">
+      <h1>Reading List</h1>
+      <BookList />
+    </div>
+  );
 }
 
 export default App;
