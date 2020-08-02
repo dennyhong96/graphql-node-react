c987fb77b7b5b40391541317314416b25c1970b8
Haiyang Hong <hong961127@gmail.com>
2020-08-02 16:29:47 -0700

    Setup apolloclient

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+(async () => {
+  const result = await client.query({
+    query: gql`
+      query {
+        books {
+          name
+        }
+      }
+    `,
+  });
+  console.log(result);
+})();
 
 ReactDOM.render(
   <React.StrictMode>
